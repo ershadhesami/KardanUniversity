496dc45d38c2c2e005b16fb53b5601e08013c4f5
Ahmad Ershad Hesami <50484645+ershadhesami@users.noreply.github.com>
2020-10-05 23:00:26 +0430

    Create find_avg.py

---
+++ find_avg.py
@@ -0,0 +1,9 @@
+# Ahmad Ershad 202-1904059
+# First Program
+num_list = []
+for the_item in range(10):
+	num_list.append(the_item)
+print(num_list)
+total_list = sum(num_list)
+avg_list = total_list / 10
+print("Total is {} and Average is {}".format(total_list, avg_list))
